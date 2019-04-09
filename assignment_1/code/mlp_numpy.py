c55e326610abf10e9970b5e63f292055a202188b
Micha de Groot <git@michadegroot.nl>
2019-04-09 20:43:29 +0200

    Finished mlp, started training

--- assignment_1/code/mlp_numpy.py
+++ assignment_1/code/mlp_numpy.py
@@ -9,79 +9,68 @@ from __future__ import print_function
 from modules import * 
 
 class MLP(object):
-  """
-  This class implements a Multi-layer Perceptron in NumPy.
-  It handles the different layers and parameters of the model.
-  Once initialized an MLP object can perform forward and backward.
-  """
-
-  def __init__(self, n_inputs, n_hidden, n_classes):
     """
-    Initializes MLP object. 
-    
-    Args:
-      n_inputs: number of inputs.
-      n_hidden: list of ints, specifies the number of units
-                in each linear layer. If the list is empty, the MLP
-                will not have any linear layers, and the model
-                will simply perform a multinomial logistic regression.
-      n_classes: number of classes of the classification problem.
-                 This number is required in order to specify the
-                 output dimensions of the MLP
-    
-    TODO:
-    Implement initialization of the network.
+    This class implements a Multi-layer Perceptron in NumPy.
+    It handles the different layers and parameters of the model.
+    Once initialized an MLP object can perform forward and backward.
     """
 
-    ########################
-    # PUT YOUR CODE HERE  #
-    #######################
-    raise NotImplementedError
-    ########################
-    # END OF YOUR CODE    #
-    #######################
+    def __init__(self, n_inputs, n_hidden, n_classes):
+        """
+        Initializes MLP object. 
+        
+        Args:
+          n_inputs: number of inputs.
+          n_hidden: list of ints, specifies the number of units
+                    in each linear layer. If the list is empty, the MLP
+                    will not have any linear layers, and the model
+                    will simply perform a multinomial logistic regression.
+          n_classes: number of classes of the classification problem.
+                     This number is required in order to specify the
+                     output dimensions of the MLP
+        """
+  
+        self.layers = []
+        for i in range(len(n_hidden)):
+            if i == 0:
+                self.layers.append(LinearModule(n_inputs, n_hidden[i]))
+            else:
+                self.layers.append(LinearModule(n_hidden[i - 1], n_hidden[i]))
+            self.layers.append(ReLUModule())
+  
+        if not self.layers:
+            self.layers.append(LinearModule(n_inputs, n_classes))
+        else:
+            self.layers.append(LinearModule(n_hidden[-1], n_classes))
+  
+        self.layers.append(SoftMaxModule())
+        #self.layers.append(CrossEntropyModule())
 
-  def forward(self, x):
-    """
-    Performs forward pass of the input. Here an input tensor x is transformed through 
-    several layer transformations.
-    
-    Args:
-      x: input to the network
-    Returns:
-      out: outputs of the network
-    
-    TODO:
-    Implement forward pass of the network.
-    """
+    def forward(self, x):
+        """
+        Performs forward pass of the input. Here an input tensor x is transformed through 
+        several layer transformations.
+        
+        Args:
+          x: input to the network
+        Returns:
+          out: outputs of the network
+        """
+        out = self.layers[0].forward(x)
+  
+        for layer in self.layers[1:]:
+            out = layer.forward(out)
+        return out
 
-    ########################
-    # PUT YOUR CODE HERE  #
-    #######################
-    raise NotImplementedError
-    ########################
-    # END OF YOUR CODE    #
-    #######################
-
-    return out
-
-  def backward(self, dout):
-    """
-    Performs backward pass given the gradients of the loss. 
-
-    Args:
-      dout: gradients of the loss
-    
-    TODO:
-    Implement backward pass of the network.
-    """
-    
-    ########################
-    # PUT YOUR CODE HERE  #
-    #######################
-    raise NotImplementedError
-    ########################
-    # END OF YOUR CODE    #
-    #######################
-
-    return
+    def backward(self, dout):
+        """
+        Performs backward pass given the gradients of the loss. 
+  
+        Args:
+          dout: gradients of the loss
+        """
+        
+        for layer in reversed(self.layers):
+            dout = layer.backward(dout)
+  
+        return dout
--- assignment_1/code/train_mlp_numpy.py
+++ assignment_1/code/train_mlp_numpy.py
@@ -26,96 +26,105 @@ DATA_DIR_DEFAULT = './cifar10/cifar-10-batches-py'
 FLAGS = None
 
 def accuracy(predictions, targets):
-  """
-  Computes the prediction accuracy, i.e. the average of correct predictions
-  of the network.
-  
-  Args:
-    predictions: 2D float array of size [batch_size, n_classes]
-    labels: 2D int array of size [batch_size, n_classes]
-            with one-hot encoding. Ground truth labels for
-            each sample in the batch
-  Returns:
-    accuracy: scalar float, the accuracy of predictions,
-              i.e. the average correct predictions over the whole batch
-  
-  TODO:
-  Implement accuracy computation.
-  """
+    """
+    Computes the prediction accuracy, i.e. the average of correct predictions
+    of the network.
+    
+    Args:
+      predictions: 2D float array of size [batch_size, n_classes]
+      labels: 2D int array of size [batch_size, n_classes]
+              with one-hot encoding. Ground truth labels for
+              each sample in the batch
+    Returns:
+      accuracy: scalar float, the accuracy of predictions,
+                i.e. the average correct predictions over the whole batch
+    
+    TODO:
+    Implement accuracy computation.
+    """
 
-  ########################
-  # PUT YOUR CODE HERE  #
-  #######################
-  raise NotImplementedError
-  ########################
-  # END OF YOUR CODE    #
-  #######################
+    ########################
+    # PUT YOUR CODE HERE  #
+    #######################
+    raise NotImplementedError
+    ########################
+    # END OF YOUR CODE    #
+    #######################
 
-  return accuracy
+    return accuracy
 
 def train():
-  """
-  Performs training and evaluation of MLP model. 
+    """
+    Performs training and evaluation of MLP model. 
+  
+    TODO:
+    Implement training and evaluation of MLP model. Evaluate your model on the whole test set each eval_freq iterations.
+    """
+  
+    ### DO NOT CHANGE SEEDS!
+    # Set the random seeds for reproducibility
+    np.random.seed(42)
+  
+    ## Prepare all functions
+    # Get number of units in each hidden layer specified in the string such as 100,100
+    if FLAGS.dnn_hidden_units:
+        dnn_hidden_units = FLAGS.dnn_hidden_units.split(",")
+        dnn_hidden_units = [int(dnn_hidden_unit_) for dnn_hidden_unit_ in dnn_hidden_units]
+    else:
+        dnn_hidden_units = []
+  
+    cifar10 = cifar10_utils.get_cifar10(FLAGS.data_dir)
 
-  TODO:
-  Implement training and evaluation of MLP model. Evaluate your model on the whole test set each eval_freq iterations.
-  """
+    n_inputs = 3*32*32
+    n_classes = 10
+    model = MLP(n_inputs, dnn_hidden_units, n_classes)
 
-  ### DO NOT CHANGE SEEDS!
-  # Set the random seeds for reproducibility
-  np.random.seed(42)
-
-  ## Prepare all functions
-  # Get number of units in each hidden layer specified in the string such as 100,100
-  if FLAGS.dnn_hidden_units:
-    dnn_hidden_units = FLAGS.dnn_hidden_units.split(",")
-    dnn_hidden_units = [int(dnn_hidden_unit_) for dnn_hidden_unit_ in dnn_hidden_units]
-  else:
-    dnn_hidden_units = []
-
-  ########################
-  # PUT YOUR CODE HERE  #
-  #######################
-  raise NotImplementedError
-  ########################
-  # END OF YOUR CODE    #
-  #######################
+    for i in range(FLAGS.max_steps):
+        batch = cifar10['train'].next_batch(FLAGS.batch_size)
+        b_s = batch[0].shape)
+        batch[0] #TODO
+        print(batch[1].shape)
+        #predictions = MLP.forward(batch)
+        break
+        # loss
+        # backwards
+    # evaluate when eval_freq
 
 def print_flags():
-  """
-  Prints all entries in FLAGS variable.
-  """
-  for key, value in vars(FLAGS).items():
-    print(key + ' : ' + str(value))
+    """
+    Prints all entries in FLAGS variable.
+    """
+    for key, value in vars(FLAGS).items():
+        print(key + ' : ' + str(value))
 
 def main():
-  """
-  Main function
-  """
-  # Print all Flags to confirm parameter settings
-  print_flags()
-
-  if not os.path.exists(FLAGS.data_dir):
-    os.makedirs(FLAGS.data_dir)
-
-  # Run the training operation
-  train()
+    """
+    Main function
+    """
+    # Print all Flags to confirm parameter settings
+    print_flags()
+  
+    if not os.path.exists(FLAGS.data_dir):
+      os.makedirs(FLAGS.data_dir)
+  
+    # Run the training operation
+    train()
 
 if __name__ == '__main__':
-  # Command line arguments
-  parser = argparse.ArgumentParser()
-  parser.add_argument('--dnn_hidden_units', type = str, default = DNN_HIDDEN_UNITS_DEFAULT,
-                      help='Comma separated list of number of units in each hidden layer')
-  parser.add_argument('--learning_rate', type = float, default = LEARNING_RATE_DEFAULT,
-                      help='Learning rate')
-  parser.add_argument('--max_steps', type = int, default = MAX_STEPS_DEFAULT,
-                      help='Number of steps to run trainer.')
-  parser.add_argument('--batch_size', type = int, default = BATCH_SIZE_DEFAULT,
-                      help='Batch size to run trainer.')
-  parser.add_argument('--eval_freq', type=int, default=EVAL_FREQ_DEFAULT,
-                        help='Frequency of evaluation on the test set')
-  parser.add_argument('--data_dir', type = str, default = DATA_DIR_DEFAULT,
-                      help='Directory for storing input data')
-  FLAGS, unparsed = parser.parse_known_args()
-
-  main()
+    # Command line arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--dnn_hidden_units', type = str, default = DNN_HIDDEN_UNITS_DEFAULT,
+                        help='Comma separated list of number of units in each hidden layer')
+    parser.add_argument('--learning_rate', type = float, default = LEARNING_RATE_DEFAULT,
+                        help='Learning rate')
+    parser.add_argument('--max_steps', type = int, default = MAX_STEPS_DEFAULT,
+                        help='Number of steps to run trainer.')
+    parser.add_argument('--batch_size', type = int, default = BATCH_SIZE_DEFAULT,
+                        help='Batch size to run trainer.')
+    parser.add_argument('--eval_freq', type=int, default=EVAL_FREQ_DEFAULT,
+                          help='Frequency of evaluation on the test set')
+    parser.add_argument('--data_dir', type = str, default = DATA_DIR_DEFAULT,
+                        help='Directory for storing input data')
+    FLAGS, unparsed = parser.parse_known_args()
+  
+    main()
