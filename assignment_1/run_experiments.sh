58dbbc8e6f9c8e5696e6c3c94272e04a5fa2abc2
Micha de Groot <git@michadegroot.nl>
2019-04-17 23:44:08 +0200

    updated job script

--- assignment_1/run_experiments.sh
+++ assignment_1/run_experiments.sh
@@ -4,19 +4,22 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=3
 #SBATCH --ntasks-per-node=1
-#SBATCH --time=0:20:00
+#SBATCH --time=0:05:00
 #SBATCH --mem=6000M
 #SBATCH --partition=gpu_shared_course
 #SBATCH --gres=gpu:1
 
 module load Python/3.6.3-foss-2017b
 
-CURR_PROG=train_mlp_numpy
+CURR_PROG=train_mlp_pytorch
+LEARNING_RATE=1e-5
+MAX_STEPS=3000
+HIDDEN_UNITS="100, 100, 100, 100"
 
 cp -r $HOME/Deep_Learning/assignment_1/code $TMPDIR
 cd $TMPDIR/code
 
-srun python3 $TMPDIR/code/$CURR_PROG.py > $CURR_PROG-results.out
+srun python3 $TMPDIR/code/$CURR_PROG.py --learning_rate $LEARNING_RATE --max_steps $MAX_STEPS --dnn_hidden_units $HIDDEN_UNITS > $CURR_PROG-results.out
 
 cp $CURR_PROG-results.out $HOME/Deep_Learning/assignment_1/results
 cp $HOME/Deep_Learning/assignment_1/slurm*.out $HOME/Deep_Learning/assignment_1/results/
