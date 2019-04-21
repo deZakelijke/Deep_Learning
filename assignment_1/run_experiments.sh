24465ece95bf07966fe8549ee12dd6b1efa679d2
Micha de Groot <git@michadegroot.nl>
2019-04-21 13:52:56 +0200

    Fixed job script

--- assignment_1/run_experiments.sh
+++ assignment_1/run_experiments.sh
@@ -1,25 +1,29 @@
 #!/bin/bash
 
-#SBATCH --job-name=example
+#SBATCH --job-name=python_experiment
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=3
 #SBATCH --ntasks-per-node=1
-#SBATCH --time=0:05:00
+#SBATCH --time=0:15:00
 #SBATCH --mem=6000M
 #SBATCH --partition=gpu_shared_course
 #SBATCH --gres=gpu:1
 
 module load Python/3.6.3-foss-2017b
 
+#CURR_PROG=train_convnet_pytorch
 CURR_PROG=train_mlp_pytorch
-LEARNING_RATE=1e-5
-MAX_STEPS=3000
-HIDDEN_UNITS="100,100,100,100"
+LEARNING_RATE=1e-4
+MAX_STEPS=4000
+HIDDEN_UNITS="400,400,300,300,300"
+BATCH_SIZE=400
+OPTIM="Adam"
 
 cp -r $HOME/Deep_Learning/assignment_1/code $TMPDIR
 cd $TMPDIR/code
 
-srun python3 $TMPDIR/code/$CURR_PROG.py --learning_rate $LEARNING_RATE --max_steps $MAX_STEPS --dnn_hidden_units $HIDDEN_UNITS > $CURR_PROG-results.out
+srun python3 $TMPDIR/code/$CURR_PROG.py --learning_rate $LEARNING_RATE --max_steps $MAX_STEPS --dnn_hidden_units $HIDDEN_UNITS --batch_norm --optim $OPTIM --batch_size $BATCH_SIZE> $CURR_PROG-results.out
+#srun python3 $TMPDIR/code/$CURR_PROG.py --learning_rate $LEARNING_RATE --max_steps $MAX_STEPS  --batch_size $BATCH_SIZE > $CURR_PROG-results.out
 
 cp $CURR_PROG-results.out $HOME/Deep_Learning/assignment_1/results
 cp *.pdf $HOME/Deep_Learning/assignment_1/results
