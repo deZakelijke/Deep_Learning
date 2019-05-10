4ac8efcbef98c4f2f73dd72ce6781366c23f0d6b
Micha de Groot <git@michadegroot.nl>
2019-05-10 14:55:33 +0200

    Started 3

---
+++ assignment_3/code/a3_vae_template.py
@@ -0,0 +1,172 @@
+import argparse
+
+import torch
+import torch.nn as nn
+import matplotlib.pyplot as plt
+from torchvision.utils import make_grid
+
+from datasets.bmnist import bmnist
+
+
+MNIST_SIZE = 28 * 28
+
+class Encoder(nn.Module):
+
+    def __init__(self, hidden_dim=500, z_dim=20):
+        super().__init__()
+
+        self.fc_1   = nn.Linear(MNIST_SIZE, hidden_dim)
+        self.fc_mu  = nn.Linear(hidden_dim, z_dim)
+        self.fc_std = nn.Linear(hidden_dim, z_dim)
+        self.tanh   = nn.Tanh()
+
+    def forward(self, input):
+        """
+        Perform forward pass of encoder.
+
+        Returns mean and std with shape [batch_size, z_dim]. Make sure
+        that any constraints are enforced.
+        """
+        h1 = self.tanh(self.fc_1(input))
+
+        mean = self.fc_mu(h1)
+        std = self.fc_std(h1)
+
+        return mean, std
+
+
+class Decoder(nn.Module):
+
+    def __init__(self, hidden_dim=500, z_dim=20):
+        super().__init__()
+
+        self.fc_1 = nn.Linear(z_dim, hidden_dim)
+        self.fc_2 = nn.Linear(hidden_dim, MNIST_SIZE)
+        self.tanh = nn.Tanh()
+
+    def forward(self, input):
+        """
+        Perform forward pass of encoder.
+
+        Returns mean with shape [batch_size, 784].
+        """
+        h1 = self.tanh(self.fc_1(input))
+        mean = self.fc_2(h1)
+
+        return mean
+
+
+class VAE(nn.Module):
+
+    def __init__(self, hidden_dim=500, z_dim=20):
+        super().__init__()
+
+        self.z_dim = z_dim
+        self.encoder = Encoder(hidden_dim, z_dim)
+        self.decoder = Decoder(hidden_dim, z_dim)
+
+    def forward(self, input):
+        """
+        Given input, perform an encoding and decoding step and return the
+        negative average elbo for the given batch.
+        """
+        mu, std = self.Encoder(input)
+        z = self.reparameterize(mu, std)
+        recon_input = self.Decoder(z)
+
+        average_negative_elbo = None
+        return average_negative_elbo
+
+    def reparameterize(self, mu, std):
+        eps = torch.FloatTensor(std.new(std.size()).normal_())
+        return eps.mul(std).add_(mu)
+
+    def sample(self, n_samples):
+        """
+        Sample n_samples from the model. Return both the sampled images
+        (from bernoulli) and the means for these bernoullis (as these are
+        used to plot the data manifold).
+        """
+        z = torch.FloatTensor((n_samples, self.z_dim))
+        im_means = self.Decoder(z)
+        sampled_ims = torch.bernoulli(im_means)
+
+        return sampled_ims, im_means
+
+
+def epoch_iter(model, data, optimizer):
+    """
+    Perform a single epoch for either the training or validation.
+    use model.training to determine if in 'training mode' or not.
+
+    Returns the average elbo for the complete epoch.
+    """
+    average_epoch_elbo = None
+    raise NotImplementedError()
+
+    return average_epoch_elbo
+
+
+def run_epoch(model, data, optimizer):
+    """
+    Run a train and validation epoch and return average elbo for each.
+    """
+    traindata, valdata = data
+
+    model.train()
+    train_elbo = epoch_iter(model, traindata, optimizer)
+
+    model.eval()
+    val_elbo = epoch_iter(model, valdata, optimizer)
+
+    return train_elbo, val_elbo
+
+
+def save_elbo_plot(train_curve, val_curve, filename):
+    plt.figure(figsize=(12, 6))
+    plt.plot(train_curve, label='train elbo')
+    plt.plot(val_curve, label='validation elbo')
+    plt.legend()
+    plt.xlabel('epochs')
+    plt.ylabel('ELBO')
+    plt.tight_layout()
+    plt.savefig(filename)
+
+
+def main():
+    data = bmnist()[:2]  # ignore test split
+    model = VAE(z_dim=ARGS.zdim)
+    optimizer = torch.optim.Adam(model.parameters())
+
+    train_curve, val_curve = [], []
+    for epoch in range(ARGS.epochs):
+        elbos = run_epoch(model, data, optimizer)
+        train_elbo, val_elbo = elbos
+        train_curve.append(train_elbo)
+        val_curve.append(val_elbo)
+        print(f"[Epoch {epoch}] train elbo: {train_elbo} val_elbo: {val_elbo}")
+
+        # --------------------------------------------------------------------
+        #  Add functionality to plot samples from model during training.
+        #  You can use the make_grid functioanlity that is already imported.
+        # --------------------------------------------------------------------
+
+    # --------------------------------------------------------------------
+    #  Add functionality to plot plot the learned data manifold after
+    #  if required (i.e., if zdim == 2). You can use the make_grid
+    #  functionality that is already imported.
+    # --------------------------------------------------------------------
+
+    save_elbo_plot(train_curve, val_curve, 'elbo.pdf')
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--epochs', default=40, type=int,
+                        help='max number of epochs')
+    parser.add_argument('--zdim', default=20, type=int,
+                        help='dimensionality of latent space')
+
+    ARGS = parser.parse_args()
+
+    main()
